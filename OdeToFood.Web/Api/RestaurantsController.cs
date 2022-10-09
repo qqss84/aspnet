332b651cfe8172f81f608a2bc6dbe13ace96a0f7
Siqi <qqss84@gmail.com>
2022-10-10 00:34:07 +0200

    add apiController of autofacé

---
+++ OdeToFood.Web/Api/RestaurantsController.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OdeToFood.Web.Api
+{
+    public class RestaurantsController : ApiController
+    {
+        private readonly IRestaurantData _db;
+        public RestaurantsController(IRestaurantData db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Restaurant> Get()
+        {
+            var model = _db.GetAll();
+            return model;
+        }
+    }
+}
--- OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -1,22 +1,26 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using OdeToFood.Data.Services;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace OdeToFood.Web
 {
     public class ContainerConfig
     {
-        internal static void RegisterContainer()
+        internal static void RegisterContainer(HttpConfiguration httpConfiguration)
         {
             var builder = new ContainerBuilder();
 
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterApiControllers(typeof(MvcApplication).Assembly);
             builder.RegisterType<InMemoryRestaurantData>().As<IRestaurantData>()
                 .SingleInstance();
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
 
         }
     }
