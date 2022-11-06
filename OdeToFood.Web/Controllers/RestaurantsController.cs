e5ca0140592f946e21ec4a5b73e7ec4db35829ff
Siqi <qqss84@gmail.com>
2022-11-06 23:00:04 +0100

    passing data across requests

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -70,6 +70,7 @@ namespace OdeToFood.Web.Controllers
             if (ModelState.IsValid)
             {
                 _db.Update(restaurant);
+                TempData["Message"] = "You have saved the restaurant!";
                 return RedirectToAction("Details", new { id = restaurant.Id });
             }
 
